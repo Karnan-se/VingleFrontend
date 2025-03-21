4557b537278844c727d08c35bed71129bc69ef6b
karnan <karnan.se@gmail.com>
2025-03-22 05:16:48 +0530

    fetch course

--- src/generalParts/tutorprofilePage/coursetable.jsx
+++ src/generalParts/tutorprofilePage/coursetable.jsx
@@ -26,9 +26,10 @@ export default function CourseTable() {
         console.log(error)
         
       }
-     fetchcourse()
+    
   
     }
+    fetchcourse()
     return ()=>fetchcourse()
    
   

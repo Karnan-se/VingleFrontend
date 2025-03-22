78e0aae2aa53af7d08e585871b4e8c94aaaac25b
karnan <karnan.se@gmail.com>
2025-03-22 11:23:30 +0530

    hello

--- src/features/api/createCourse.js
+++ src/features/api/createCourse.js
@@ -5,12 +5,10 @@ import swal from "sweetalert"
 export const createCourse = async (form) =>{
  try {
     const response = await tutorApi.post("/createCourse",form,{
-      withCredentials:true,
       headers : {
           "Content-Type": "multipart/form-data", 
+      },
       
-
-      }
     } ,)
       console.log(response , "response")
       if(response){

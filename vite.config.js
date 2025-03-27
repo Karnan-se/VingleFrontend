f6214926f1650b8e3a70aa5e982d011012dd2f8c
karnan <karnan.se@gmail.com>
2025-03-27 12:06:00 +0530

    fixed po;yufill

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,15 @@ export default defineConfig({
   plugins: [react()],
   define: {
     global: 'window',
+    "process.env": {},
   },
   optimizeDeps: {
     esbuildOptions: {
-      // Node.js global to browser globalThis
+      
       define: {
         global: 'globalThis',
       },
-      // Enable esbuild polyfill plugins
+      
       plugins: [
         NodeGlobalsPolyfillPlugin({
           buffer: true,
